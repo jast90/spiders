a9038bac5d810a649c9807c1609c5d67661867af
jast90 <zhangzhiwen91@gmail.com>
2020-06-01 21:10:42 +0800

    将目录下的所有json文件转换成json lines文件

--- price/json2jsonl.py
+++ price/json2jsonl.py
@@ -2,26 +2,32 @@
 import json
 import jsonlines
 from os import listdir
-
+import os
 # JSON to JSON lines
 
 # 将单个JSON文件转换成JSON Lines文件
-def json2jsonl(jsonFileName):
+def json2jsonl(jsonFileName,jsonlFileName):
     with open(jsonFileName) as jsonFile:
         data = json.load(jsonFile)
-        with jsonlines.open("{}l".format(jsonFileName), mode='w') as writer:
+        with jsonlines.open(jsonlFileName, mode='w') as writer:
             writer.write_all(data)
     return 
 
 # 将目录下的所有json文件转换成json lines文件
-def json2jsonlByFolder(folder,limit=3):
+def json2jsonlByFolder(folder,limit=-1):
     count = 0
     files = listdir(folder)
-    print(files) 
+    jsonlFolderName = "{}/jsonl/".format(folder)
+    if not os.path.exists(jsonlFolderName):
+        os.makedirs(jsonlFolderName)
     for file in files:
-        if(count>=limit):
+        if not file.endswith(".json"):
+            continue
+
+        if(limit != -1 and count>=limit):
             break
-        json2jsonl("{}/{}".format(folder,file))  
+
+        json2jsonl("{}/{}".format(folder,file),"{}/jsonl/{}l".format(folder,file))  
         count = count+1
     return 
 
